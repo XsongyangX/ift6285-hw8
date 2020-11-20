536d8d939ef556a1f74ceba7e5fb832bbcde4188
Song Yang <songyang1218@gmail.com>
2020-11-20 18:25:16 -0500

    Viterbi parser

--- pcfg.py
+++ pcfg.py
@@ -6,10 +6,6 @@ import pickle
 
 from nltk.tree import Tree
 
-def print_tree(set_of_sentences):
-    for tree in treebank.parsed_sents(set_of_sentences):
-        print(tree)
-
 def induce_grammar(train):
     """Induces a PCFG from the given set of Penn Treebank sentences
 
@@ -55,22 +51,36 @@ def fill_missing_words(grammar : PCFG, missing_words: Set[str]):
     adjusted_productions.append(rule)
     return PCFG(Nonterminal('S'), adjusted_productions)
 
+
+def parse_treebank(parser: PCFG, sentences):
+    for sentence in treebank.parsed_sents(sentences):
+        tokens = sentence.leaves()
+        for tree in parser.parse(tokens):
+            print(tree)
+
 def main():
     # train = treebank.fileids()[:190]
     test = treebank.fileids()[190:] # 10 last sentences
 
-    # # original grammar
+    # original grammar
     # pcfg = induce_grammar(train)
     # pickle.dump(pcfg, open("grammar.pcfg", 'wb'))
 
     # load grammar
-    pcfg : PCFG = pickle.load(open("grammar.pcfg", 'rb'))
+    # pcfg : PCFG = pickle.load(open("grammar.pcfg", 'rb'))
 
     # fill in missing words
-    missing_words = get_missing_words(pcfg, test)
-    pcfg_unk = fill_missing_words(pcfg, missing_words)
+    # missing_words = get_missing_words(pcfg, test)
+    # pcfg_unk = fill_missing_words(pcfg, missing_words)
 
-    pickle.dump(pcfg_unk, open("grammar_unk.pcfg", 'wb'))
+    # pickle.dump(pcfg_unk, open("grammar_unk.pcfg", 'wb'))
+
+    # load unk grammar
+    pcfg_unk : PCFG = pickle.load(open("grammar_unk.pcfg", 'rb'))
+
+    # use unk grammar on test sentences
+    parser = ViterbiParser(pcfg_unk)
+    parse_treebank(parser, test)
 
 if __name__ == "__main__":
     main()
