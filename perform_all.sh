ceba2ad8d9ad396b4e0dec27b523a87a9d0d4294
Song Yang <songyang1218@gmail.com>
2020-11-20 23:47:59 -0500

    Distribution script

---
+++ perform_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+jump=2
+begin=(0 2 4 6 8)
+
+for i in "${begin[@]}"; do 
+    let j=$i+$jump
+    pkscreen ssh ens -J arcade bash "cd ift6285/hw8; python performance.py $i $j > performance_$i-$j.log"
+done
