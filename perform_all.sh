6990e88b88a620e7bb8813496dc92a0ac19ad69a
Song Yang <songyang1218@gmail.com>
2020-11-20 23:51:19 -0500

    Simplify bash

--- perform_all.sh
+++ perform_all.sh
@@ -5,5 +5,5 @@ begin=(0 2 4 6 8)
 
 for i in "${begin[@]}"; do 
     let j=$i+$jump
-    pkscreen ssh ens -J arcade bash "cd ift6285/hw8; python performance.py $i $j > performance_$i-$j.log"
+    pkscreen ssh ens -J arcade "cd ift6285/hw8; python performance.py $i $j > performance_$i-$j.log"
 done
