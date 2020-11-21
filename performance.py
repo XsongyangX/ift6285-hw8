0566c9d67180ea12a119e829238d5439d16cac18
Song Yang <songyang1218@gmail.com>
2020-11-20 23:31:09 -0500

    Selective performance evaluation

--- performance.py
+++ performance.py
@@ -2,6 +2,11 @@
 from nltk.parse.viterbi import ViterbiParser
 from nltk.corpus import treebank
 import pickle
+import sys
+
+subset_begin = int(sys.argv[0])
+subset_end = int(sys.argv[1])
+assert subset_end >= subset_begin
 
 # load unk grammar
 pcfg_unk = pickle.load(open("grammar_unk.pcfg", 'rb'))
@@ -22,25 +27,32 @@ def parse(parser: ViterbiParser, sentence):
         yield tree        
 
 # predict trees
-subset = 2
-predictions = [tree.pos() for tree in parse(parser, test_sentence[:subset])]
+def get_predictions():
+    for sentence in test_sentence[subset_begin:subset_end]:
+        for tree in parse(parser, sentence):
+            yield tree.pos()
 
 # evaluate
 correct = 0
 total = 0
-for predicted, truth in zip(predictions, test_pos):
-
+for predicted, truth in zip(get_predictions(), test_pos):
+    this_sentence_correct = 0
+    this_sentence_total = 0
+    
     for (word_predicted, pos_predicted), (word, pos) in zip(predicted, truth):
         if word_predicted != word:
             raise ValueError(f"Leaves of the trees are different, prediction word: {word_predicted} vs real word: {word}")
         
         if pos_predicted == pos:
-            correct += 1
-        total += 1
+            this_sentence_correct += 1
+        this_sentence_total += 1
 
     # sentence stats
     print("Sentence length: {}".format(len(predicted)))
     print("Unknown count: {}".format(len([_ for _, pos in predicted if pos == 'UNK'])))
-    print("Sentence accuracy: {}".format(correct/total if total != 0 else 0))
+    print("--Sentence accuracy: {}".format(this_sentence_correct/this_sentence_total if total != 0 else 0))
+
+    correct += this_sentence_correct
+    total += this_sentence_total
 
 print("Total accuracy {}".format(correct/total))
